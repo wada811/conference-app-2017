115ddce458afab0956321260026b3ef5c7578ff2
wada811 <at.wada811@gmail.com>
2019-12-06 11:41:00 +0900

    Kotlinize MainActivity

--- app/src/main/java/io/github/droidkaigi/confsched2017/view/activity/MainActivity.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2017/view/activity/MainActivity.kt
@@ -1,134 +1,110 @@
-package io.github.droidkaigi.confsched2017.view.activity;
+package io.github.droidkaigi.confsched2017.view.activity
 
-import android.content.Context;
-import android.content.Intent;
-import android.databinding.DataBindingUtil;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.content.Context
+import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
+import android.view.MenuItem
+import io.github.droidkaigi.confsched2017.R
+import io.github.droidkaigi.confsched2017.databinding.ActivityMainBinding
+import io.github.droidkaigi.confsched2017.util.LocaleUtil
+import io.github.droidkaigi.confsched2017.view.fragment.InformationFragment
+import io.github.droidkaigi.confsched2017.view.fragment.MapFragment
+import io.github.droidkaigi.confsched2017.view.fragment.SessionsFragment
+import io.github.droidkaigi.confsched2017.view.fragment.SettingsFragment
+import io.github.droidkaigi.confsched2017.view.helper.BottomNavigationViewHelper
 
-import io.github.droidkaigi.confsched2017.R;
-import io.github.droidkaigi.confsched2017.databinding.ActivityMainBinding;
-import io.github.droidkaigi.confsched2017.util.LocaleUtil;
-import io.github.droidkaigi.confsched2017.view.fragment.InformationFragment;
-import io.github.droidkaigi.confsched2017.view.fragment.MapFragment;
-import io.github.droidkaigi.confsched2017.view.fragment.SessionsFragment;
-import io.github.droidkaigi.confsched2017.view.fragment.SettingsFragment;
-import io.github.droidkaigi.confsched2017.view.helper.BottomNavigationViewHelper;
-
-public class MainActivity extends BaseActivity {
-
-    private ActivityMainBinding binding;
-
-    private Fragment sessionsFragment;
-
-    private Fragment mapFragment;
-
-    private Fragment informationFragment;
-
-    private Fragment settingsFragment;
-
-    public static Intent createIntent(Context context) {
-        return new Intent(context, MainActivity.class);
+class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var sessionsFragment: Fragment? = null
+    private var mapFragment: Fragment? = null
+    private var informationFragment: Fragment? = null
+    private var settingsFragment: Fragment? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        LocaleUtil.initLocale(this)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
+        component.inject(this)
+        initView()
+        initFragments(savedInstanceState)
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        LocaleUtil.initLocale(this);
-
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        setSupportActionBar(binding.toolbar);
-        getComponent().inject(this);
-
-        initView();
-        initFragments(savedInstanceState);
-    }
-
-    private void initView() {
-        BottomNavigationViewHelper.disableShiftingMode(binding.bottomNav);
-        binding.bottomNav.setOnNavigationItemSelectedListener(item -> {
-            binding.title.setText(item.getTitle());
-            item.setChecked(true);
-            switch (item.getItemId()) {
-                case R.id.nav_sessions:
-                    switchFragment(sessionsFragment, SessionsFragment.TAG);
-                    break;
-                case R.id.nav_map:
-                    switchFragment(mapFragment, MapFragment.TAG);
-                    break;
-                case R.id.nav_information:
-                    switchFragment(informationFragment, InformationFragment.TAG);
-                    break;
-                case R.id.nav_settings:
-                    switchFragment(settingsFragment, SettingsFragment.TAG);
-                    break;
+    private fun initView() {
+        BottomNavigationViewHelper.disableShiftingMode(binding.bottomNav)
+        binding.bottomNav.setOnNavigationItemSelectedListener { item: MenuItem ->
+            binding.title.text = item.title
+            item.isChecked = true
+            when (item.itemId) {
+                R.id.nav_sessions -> switchFragment(sessionsFragment!!, SessionsFragment.TAG)
+                R.id.nav_map -> switchFragment(mapFragment!!, MapFragment.TAG)
+                R.id.nav_information -> switchFragment(informationFragment!!, InformationFragment.TAG)
+                R.id.nav_settings -> switchFragment(settingsFragment!!, SettingsFragment.TAG)
             }
-            return false;
-        });
+            false
+        }
     }
 
-    private void initFragments(Bundle savedInstanceState) {
-        final FragmentManager manager = getSupportFragmentManager();
-        sessionsFragment = manager.findFragmentByTag(SessionsFragment.TAG);
-        mapFragment = manager.findFragmentByTag(MapFragment.TAG);
-        informationFragment = manager.findFragmentByTag(InformationFragment.TAG);
-        settingsFragment = manager.findFragmentByTag(SettingsFragment.TAG);
-
+    private fun initFragments(savedInstanceState: Bundle?) {
+        val manager = supportFragmentManager
+        sessionsFragment = manager.findFragmentByTag(SessionsFragment.TAG)
+        mapFragment = manager.findFragmentByTag(MapFragment.TAG)
+        informationFragment = manager.findFragmentByTag(InformationFragment.TAG)
+        settingsFragment = manager.findFragmentByTag(SettingsFragment.TAG)
         if (sessionsFragment == null) {
-            sessionsFragment = SessionsFragment.newInstance();
+            sessionsFragment = SessionsFragment.newInstance()
         }
         if (mapFragment == null) {
-            mapFragment = MapFragment.newInstance();
+            mapFragment = MapFragment.newInstance()
         }
         if (informationFragment == null) {
-            informationFragment = InformationFragment.newInstance();
+            informationFragment = InformationFragment.newInstance()
         }
         if (settingsFragment == null) {
-            settingsFragment = SettingsFragment.newInstance();
+            settingsFragment = SettingsFragment.newInstance()
         }
-
         if (savedInstanceState == null) {
-            switchFragment(sessionsFragment, SessionsFragment.TAG);
+            switchFragment(sessionsFragment!!, SessionsFragment.TAG)
         }
     }
 
-    private boolean switchFragment(@NonNull Fragment fragment, @NonNull String tag) {
-        if (fragment.isAdded()) {
-            return false;
+    private fun switchFragment(fragment: Fragment, tag: String): Boolean {
+        if (fragment.isAdded) {
+            return false
         }
-
-        final FragmentManager manager = getSupportFragmentManager();
-        final FragmentTransaction ft = manager.beginTransaction();
-
-        final Fragment currentFragment = manager.findFragmentById(R.id.content_view);
+        val manager = supportFragmentManager
+        val ft = manager.beginTransaction()
+        val currentFragment = manager.findFragmentById(R.id.content_view)
         if (currentFragment != null) {
-            ft.detach(currentFragment);
+            ft.detach(currentFragment)
         }
-        if (fragment.isDetached()) {
-            ft.attach(fragment);
+        if (fragment.isDetached) {
+            ft.attach(fragment)
         } else {
-            ft.add(R.id.content_view, fragment, tag);
+            ft.add(R.id.content_view, fragment, tag)
         }
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                .commit();
-
+                .commit()
         // NOTE: When this method is called by user's continuous hitting at the same time,
-        // transactions are queued, so necessary to reflect commit instantly before next transaction starts.
-        manager.executePendingTransactions();
-
-        return true;
+// transactions are queued, so necessary to reflect commit instantly before next transaction starts.
+        manager.executePendingTransactions()
+        return true
     }
 
-    @Override
-    public void onBackPressed() {
-        if (switchFragment(sessionsFragment, SessionsFragment.TAG)) {
-            binding.bottomNav.getMenu().findItem(R.id.nav_sessions).setChecked(true);
-            binding.title.setText(getString(R.string.sessions));
-            return;
+    override fun onBackPressed() {
+        if (switchFragment(sessionsFragment!!, SessionsFragment.TAG)) {
+            binding.bottomNav.menu.findItem(R.id.nav_sessions).isChecked = true
+            binding.title.text = getString(R.string.sessions)
+            return
         }
-        super.onBackPressed();
+        super.onBackPressed()
+    }
+
+    companion object {
+        fun createIntent(context: Context?): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
     }
 }
